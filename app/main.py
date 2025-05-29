d10900d633f9aa7222f5a22be7be7e9e93fbd637
sails-gowthamssd <saidurga.gowtham@sailssoftware.com>
2025-05-29 15:59:55 +0530

    fdgd

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Jenkins CI build! and CD deploy using helm and testing auto trigger CI🚀"
+    return "Hello from Jenkins CI build! and CD deploy using helm and testing auto trigger CI and shown demo to bhagyaraj🚀"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
